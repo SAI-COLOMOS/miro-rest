7b11123ffb607151b47df5326b875de83f1a5858
LordSinSentido <a19310166@ceti.mx>
2022-12-31 21:54:08 -0600

    Implementaciones varias

--- app/server.js
+++ app/server.js
@@ -1,9 +1,20 @@
 /* Importaciones */
+
+//Importaciones de librerías
 import express from 'express';
+import session from "express-session";
 import morgan from 'morgan';
+import passport from "passport";
 import path from 'path';
 import { fileURLToPath } from "url";
+import conection from "./database/database.js";
+import pass from "./passport/Auth.js";
+
+// Importaciones de controladores
 import Index from "./routes/index.js";
+import Login from "./routes/auth/login.js";
+import Signup from "./routes/auth/signup.js";
+import Logout from "./routes/auth/logout.js";
 
 /* Variables */
 const app = express();
@@ -17,11 +28,22 @@ app.set('view engine', 'pug');
 
 /* Middlewares */
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: 'El Jonathan se la come :v',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* Código del servidor */
 
 // Rutas
 app.use('/', Index);
+app.use('/login', Login);
+app.use('/signup', Signup);
+app.use('/logout', Logout);
 
 // Escucha del puerto
 app.listen(app.get('port'), () => {
