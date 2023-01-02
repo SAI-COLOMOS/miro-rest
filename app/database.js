31340d3ead30bcf33d1e9be3524e4aa35cc0c1f9
LordSinSentido <a19310166@ceti.mx>
2023-01-01 21:22:37 -0600

    Se agregaron configuraciones

--- app/database.js
+++ app/database.js
@@ -5,7 +5,11 @@ mongoose.set('strictQuery', true);
 
 const conection = mongoose.connect(
     env.monogodb.URI,
-    {}
+    {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
     ).then(
         (datebase) => {
             console.info('Conectado a la base de datos');
