aed27de47e623e6d591e702722ed137f5f31ddeb
LordSinSentido <a19310166@ceti.mx>
2023-01-07 00:04:36 -0600

    Se corrigió el metodo http

--- app/routes/UsersAdmon.js
+++ app/routes/UsersAdmon.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { Register_put } from "../controllers/UsersAdmon/Register.js";
+import { Register_Post } from "../controllers/UsersAdmon/Register.js";
 import {isAuthenticated, isNotAuthenticated} from "../passport/VerifyAuth.js";
 
 const route = Router();
@@ -7,6 +7,6 @@ const path = 'users';
 
 //route.get(`/${path}`, isNotAuthenticated, Register.Get);
 //route.post(`/${path}`, isNotAuthenticated, Post);
-route.put(`/${path}/register`, Register_put);
+route.post(`/${path}/register`, Register_Post);
 
 export default route;
