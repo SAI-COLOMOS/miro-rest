fa9fbc4b5e66b6c8e167d091fcc6f403b6ae8152
JonathanGDiaz <morokeidovah@gmail.com>
2023-01-10 23:46:14 -0600

    Se añadió la ruta para la recuperación de la contraseña

--- app/routes/Auth.routes.ts
+++ app/routes/Auth.routes.ts
@@ -1,10 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 import { LoginPost, RegisterPost } from "../controllers/Auth.controller";
+import { RecoverPassword } from "../controllers/PasswordRecovery.controller";
 
 const routes = Router();
 
 routes.post('/login', LoginPost);
 
 routes.post('/register', RegisterPost);
+
+routes.post('/recoverPassword', RecoverPassword);
 
 export default routes;
