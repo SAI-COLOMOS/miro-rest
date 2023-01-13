eb6152a64f1f6ee3c5715d9a430c70c4cbb4a7fd
LordSinSentido <a19310166@ceti.mx>
2023-01-12 23:22:10 -0600

    Cambio en el nombre del método

--- app/controllers/UsersComponent/Users.controller.ts
+++ app/controllers/UsersComponent/Users.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import User, {UserInterface} from "../../models/User";
 
-export const UserGet = (req: Request, res: Response) => {
+export const UsersGet = (req: Request, res: Response) => {
     return res.status(200).json({
         message: "I'm a teapot",
         lo: req.user
@@ -9,18 +9,6 @@ export const UserGet = (req: Request, res: Response) => {
 }
 
 export const UsersPost = async (req: Request, res: Response) => {
-    //if(!req.body.email) {
-    //    return res.status(400).json({
-    //        message: "Faltan datos"
-    //    })
-    //}
-
-    //if(await User.findOne({email: req.body.email})) {
-    //    return res.status(400).json({
-    //        message: "Algunos datos ya están ocupados"
-    //    })
-    //}
-
     try {
         const newUser = await new User(req.body).save()
         return res.status(201).json({
@@ -30,7 +18,7 @@ export const UsersPost = async (req: Request, res: Response) => {
     } catch (error: any) {
         return res.status(500).json({
             message: "Ocurrió un error",
-            error: error._message
+            error: error
         })
     }
 }
--- app/routes/UsersComponent.routes.ts
+++ app/routes/UsersComponent.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { UserGet, UsersPost } from "../controllers/UsersComponent/Users.controller"
+import { UsersGet, UsersPost } from "../controllers/UsersComponent/Users.controller"
 import { isAdministradorOrEncargado } from "../middleware/RoleControl"
 import Passport from "passport"
 
@@ -9,7 +9,7 @@ const path = 'users'
 //route.use(Passport.authenticate('jwt',{session: false}))
 //route.use(isAdministradorOrEncargado);
 
-route.get(`/${path}`, UserGet)
+route.get(`/${path}`, UsersGet)
 
 route.post(`/${path}`, UsersPost)
 
