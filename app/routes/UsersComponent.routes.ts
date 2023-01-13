e0282503b292bbf4cbd670cb4b541b7c99a83d8d
LordSinSentido <a19310166@ceti.mx>
2023-01-12 20:11:02 -0600

    Anexión de ruta para agregar usuarios

--- app/routes/UsersComponent.routes.ts
+++ app/routes/UsersComponent.routes.ts
@@ -6,8 +6,8 @@ import Passport from "passport"
 const route = Router()
 const path = 'users'
 
-route.use(Passport.authenticate('jwt',{session: false}))
-route.use(isAdministradorOrEncargado);
+//route.use(Passport.authenticate('jwt',{session: false}))
+//route.use(isAdministradorOrEncargado);
 
 route.get(`/${path}`, UserGet)
 
