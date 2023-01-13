edc01180ecefc5292226ce1e2fd11eda42738776
JonathanGDiaz <morokeidovah@gmail.com>
2023-01-13 00:38:04 -0600

    Se eliminó el secret innecesario

--- app/config/Enviroment.ts
+++ app/config/Enviroment.ts
@@ -1,7 +1,6 @@
 export default {
     JWT: {
-        secret: "70d45581874c292eb0a7b74ff29890b6",
-        recoverySecret: "12eeef04c5820b6595753b2c09a269b5"
+        secret: "70d45581874c292eb0a7b74ff29890b6"
     },
     MonogoDB: {
         uri: 'mongodb://127.0.0.1:27017/sai_database'
