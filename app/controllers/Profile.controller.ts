2a8a6d601202f5f35ae6b6b862717f45b380a143
LordSinSentido <a19310166@ceti.mx>
2023-01-13 23:39:59 -0600

    Ruta y controlador creados para el perfil de usuario

---
+++ app/controllers/Profile.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express"
+import User, {UserInterface} from "../models/User";
+
+export const ProfileGet = (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ app/routes/Profile.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import { ProfileGet } from "../controllers/Profile.controller";
+
+const route = Router()
+const path = 'profile'
+
+//route.use(Passport.authenticate('jwt',{session: false}))
+//route.use(isAdministradorOrEncargado);
+
+route.get(`/${path}/:id`, ProfileGet)
+//route.patch(`/${path}/:id`, UserPatch)
+
+export default route
