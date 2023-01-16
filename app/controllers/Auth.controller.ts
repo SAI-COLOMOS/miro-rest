5a166707669a6a740857153718bf647d056c3d43
JonathanGDiaz <morokeidovah@gmail.com>
2023-01-15 22:18:40 -0600

    Se terminó de implementar la verificación por correo

--- app/controllers/Auth.controller.ts
+++ app/controllers/Auth.controller.ts
@@ -3,153 +3,114 @@ import User, { UserInterface } from "../models/User";
 import JWT, { JwtPayload } from "jsonwebtoken";
 import Enviroment from "../config/Enviroment";
 import { link, sendEmail } from "../config/Mailer";
-import { Request, Response } from "express"
-import User, { UserInterface } from "../models/User"
-import JWT from "jsonwebtoken"
-import Enviroment from "../config/Enviroment"
 
-function createToken(user: UserInterface) {
+function createToken(user: UserInterface, time: String) {
     return JWT.sign({
-        id: user.id,
+        id: user.id
     },
         Enviroment.JWT.secret,
         {
-            expiresIn: "90d"
+            expiresIn: String(time)
         })
 }
 
 export const LoginPost = async (req: Request, res: Response) => {
-    if (!req.body.email) {
+    if (!req.body.credential || !req.body.password) {
         return res.status(400).json({
             message: "Faltan datos"
         })
     }
-
-    const user = await User.findOne({ email: req.body.email })
-
-    if (!user) {
-        return res.status(400).json({
-            message: "Usuario no encontrado"
-        })
-    }
-
-    if (await user.validatePassword(req.body.password)) {
-        return res.status(200).json({
-            message: "Sesión iniciada",
-            token: createToken(user)
-        })
-    } else {
-        return res.status(400).json({
-            message: "Falló el inicio de sesión"
-        })
-    }
-}
-
-export const RegisterPost = async (req: Request, res: Response): Promise<Response> => {
-    if (!req.body.email) {
-        return res.status(400).json({
-            message: "Faltan datos"
-        })
-    }
-
-    if (await User.findOne({ email: req.body.email })) {
-        return res.status(400).json({
-            message: "Algunos datos ya están ocupados"
-        })
-    }
-
+    let user
     try {
-        const newUser = await new User(req.body).save()
-        return res.status(201).json({
-            message: "Usuario creado",
-            data: newUser
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: "Ocurrió un error",
-            error: error
-        })
-    }
-}
-
-export const sendRecoveryToken = async (req: Request, res: Response) => {
-
-    try {
-        var user = await User.findOne({ email: req.body.email, phone: req.body.phone, register: req.body.register }).sort({ "register": "desc" })
-
+        if (req.body.credential.search('@') !== -1) {
+            user = await User.findOne({ email: req.body.credential }).sort({ "register": "desc" })
+        } else if (!Number.isNaN(req.body.credential) && req.body.credential.length === 10) {
+            user = await User.findOne({ phone: req.body.credential }).sort({ "register": "desc" })
+        } else {
+            user = await User.findOne({ register: req.body.credential }).sort({ "register": "desc" })
+        }
         if (!user) {
-            return res.status(200).json({
-                status: 406,
-                message: "Usuario no encontrado"
+            return res.status(404).json({
+                message: "No se encontró ningún usuario"
+            })
+        }
+        if (!await user.validatePassword(req.body.password)) {
+            return res.status(400).json({
+                message: "Falló el inicio de sesión, la contraseña o el usuario es incorrecto"
             })
         }
     } catch (error) {
         return res.status(500).json({
-            message: "Ocurrió un error",
-            error: error
+            message: "Ocurrió un error al connectarse con el servidor",
+            error
         })
     }
 
-    const token = createToken(user)
-    const newRoute = `localhost:3000/recovery/changePassword?tkn=${token}`
+    return res.status(200).json({
+        message: "Sesión iniciada",
+        token: createToken(user, "7d")
+    })
+}
 
+export const sendRecoveryToken = async (req: Request, res: Response) => {
+    let user, newRoute
     try {
-        // const from = `"Recuperación de contraseña SAI" ${Enviroment.Mailer.email}`
-        // const to = "a19310166@ceti.mx"
-        // const subject = "Recuperación de contraseña"
-        // const body = link(newRoute)
-
-        // sendEmail(from, to, subject, body)
-
+        if (req.body.credential.search('@') !== -1) {
+            user = await User.findOne({ email: req.body.credential }).sort({ "register": "desc" })
+        } else if (!Number.isNaN(req.body.credential) && req.body.credential.length === 10) {
+            user = await User.findOne({ phone: req.body.credential }).sort({ "register": "desc" })
+        } else {
+            user = await User.findOne({ register: req.body.credential }).sort({ "register": "desc" })
+        }
+        if (!user) {
+            return res.status(404).json({
+                message: "No se encontró ningún usuario"
+            })
+        }
+        const token = createToken(user, "5m")
+        newRoute = `localhost:3000/auth/changePassword?tkn=${token}`
+        const from = `"Recuperación de contraseña SAI" ${Enviroment.Mailer.email}`
+        const to = String(user.email)
+        const subject = "Recuperación de contraseña"
+        const body = link(newRoute)
+        await sendEmail(from, to, subject, body)
     } catch (error) {
         return res.status(500).json({
-            message: "Ocurrió un error",
-            error: error
+            message: "Ocurrió un error al connectarse con el servidor",
+            error
         })
     }
 
     return res.status(200).json({
         message: "Correo enviado",
-        route: newRoute
     })
 }
 
 export const recoverPassword = async (req: Request, res: Response) => {
+    let token, user
     try {
-        var token = JWT.verify(String(req.query.tkn), Enviroment.JWT.secret) as JwtPayload
-    } catch (error) {
-        return res.status(500).json({
-            message: "El link ha caducado"
-        })
-    }
-
-    try {
-        var user = await User.findOne({ id: token.id }).sort({ "register": "desc" })
-        if (!user) {
-            return res.status(200).json({
-                status: 406,
-                message: "Usuario no encontrado"
+        try {
+            token = JWT.verify(String(req.query.tkn), Enviroment.JWT.secret) as JwtPayload
+        } catch (error) {
+            return res.status(400).json({
+                message: "El link ha caducado",
             })
         }
-    } catch (error) {
-        return res.status(500).json({
-            message: "Ocurrió un error"
-        })
-    }
-
-    try {
+        user = await User.findById(token.id)
+        if (!user) {
+            return res.status(404).json({
+                message: "Usuario no encontrado",
+            })
+        }
         user.password = req.body.password
         await user.save()
     } catch (error) {
         return res.status(500).json({
-            message: "Ocurrió un error"
+            message: "Ocurrió un error al connectarse con el servidor"
         })
     }
 
     return res.status(200).json({
-        message: "Contraseña actualizada con éxico",
-        data: {
-            tkn: token.id
-        }
+        message: "Contraseña actualizada con éxito",
     })
 }
