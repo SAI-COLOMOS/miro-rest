69baeb173540462ff486d349e151704ba469eaf8
LordSinSentido <a19310166@ceti.mx>
2023-01-17 21:27:14 -0600

    Commit de emergencia

---
+++ app/routes/Profile.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express"
+import { ProfileGet } from "../controllers/Profile.controller";
+import Passport from "passport";
+
+const route = Router()
+const path = 'profile'
+
+route.use(Passport.authenticate('jwt',{session: false}))
+//route.use(isAdministradorOrEncargado);
+
+route.get(`/${path}/:id`, ProfileGet)
+//route.patch(`/${path}/:id`, UserPatch)
+
+export default route
