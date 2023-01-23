e1947d82688e8a3c668d61838094e9f886d8c9bf
LordSinSentido <a19310166@ceti.mx>
2023-01-22 18:14:55 -0600

    Cambio en la lógica

--- app/controllers/Users.controller.ts
+++ app/controllers/Users.controller.ts
@@ -6,22 +6,33 @@ export const UsersGet = async (req: Request, res: Response) => {
         const items: number = req.body.items > 0 ? req.body.items : 10
         const page: number = req.body.page > 0 ? req.body.page - 1 : 0
         
-        const users = await User.find().limit(items).skip(page * items)
+        await User.find().sort({"createdAt": "desc"}).limit(items).skip(page * items).then(
+            (result) => {
+                if(result.length > 0) {
+                    return res.status(200).json({
+                        message: "Listo",
+                        result
+                    })
+                } else {
+                    res.status(200).json({
+                        message: `Sin resultados`
+                    })
+                }
+            }
+        ).catch(
+            (error) => {
+                return res.status(500).json({
+                    message: "Ocurrió un error interno con la base de datos",
+                    error: error?.toString()
+                })
+            }
+        )
 
-        if(users.length > 0) {
-            return res.status(200).json({
-                message: "Listo",
-                users
-            })
-        }
 
-        res.status(200).json({
-            message: `Sin resultados`
-        })
     } catch (error) {
         return res.status(500).json({
             message: "Ocurrió un error",
-            error
+            error: error?.toString()
         })
     }
 
@@ -29,85 +40,130 @@ export const UsersGet = async (req: Request, res: Response) => {
 
 export const UserGet = async (req: Request, res: Response) => {
     try {
-        const user = await User.findOne({'register': req.params.id})
-        if(user) {
-            return res.status(200).json({
-                message: "Listo",
-                user
-            })
-        }
-
-        res.status(404).json({
-            message: `Usuario ${req.params.id} no encontrado`
-        })
+        await User.findOne({'register': req.params.id}).then(
+            (result) => {
+                if(result) {
+                    return res.status(200).json({
+                        message: "Listo",
+                        result
+                    })
+                } else {
+                    res.status(404).json({
+                        message: `Usuario ${req.params.id} no encontrado`
+                    })
+                }
+            }
+        ).catch(
+            (error) => {
+                return res.status(500).json({
+                    message: "Ocurrió un error interno con la base de datos",
+                    error: error?.toString()
+                })
+            }
+        )
     } catch (error) {
         return res.status(500).json({
             message: "Ocurrió un error",
-            error
+            error: error?.toString()
         })
     }
 }
 
 export const UserPost = async (req: Request, res: Response) => {
     try {
-        const newUser = await new User(req.body).save()
-        return res.status(201).json({
-            message: "Usuario creado",
-            data: newUser
-        })
+        await new User(req.body).save().then(
+            (result) => {
+                if(result) {
+                    return res.status(201).json({
+                        message: "Usuario creado",
+                        data: result
+                    })
+                } else {
+                    return res.status(500).json({
+                        message: "No se pudo crear el usuario",
+                    })            
+                }
+            }
+        ).catch(
+            (error) => {
+                return res.status(500).json({
+                    message: "Ocurrió un error interno con la base de datos",
+                    error: error?.toString()
+                })
+            }
+        )
     } catch (error: any) {
         return res.status(500).json({
             message: "Ocurrió un error",
-            error: error
+            error: error?.toString()
         })
     }
 }
 
 export const UserDelete = async (req: Request, res: Response) => {
     try {
-        const user = await User.deleteOne({'register': req.params.id})
-        if(user.deletedCount !== 0) {
-            return res.status(200).json({
-                message: "Usuario eliminado",
-            })
-        }
+        await User.deleteOne({'register': req.params.id}).then(
+            (result) => {
+                if(result.deletedCount !== 0) {
+                    return res.status(200).json({
+                        message: "Usuario eliminado",
+                    })
+                } else {
+                    return res.status(404).json({
+                        message: `Usuario ${req.params.id} no encontrado`
+                    })
+                }
+            }
+        ).catch(
+            (error) => {
+                return res.status(500).json({
+                    message: "Ocurrió un error interno con la base de datos",
+                    error: error?.toString()
+                })
+            }
+        )
 
-        return res.status(404).json({
-            message: `Usuario ${req.params.id} no encontrado`
-        })
     } catch (error) {
         return res.status(500).json({
             message: "Ocurrió un error",
-            error: error
+            error: error?.toString()
         })
     }
 }
 
 export const UserPatch = async (req: Request, res: Response) => {
     try {
-        const informationUpdated = req.body.informationUpdated
-
-        if(!informationUpdated) {
+        if(req.body.password || req.body.register) {
             return res.status(400).json({
-                message: "Faltan datos"
+                message: "Algunos campos no se pueden actualizar"
             })
         }
 
-        const result = await User.updateOne({'register': req.params.id}, informationUpdated)
+        await User.updateOne({'register': req.params.id}, req.body).then(
+            (result) => {
+                if(result.modifiedCount > 0) {
+                    return res.status(200).json({
+                        message: `Se actualizó la información del usuario ${req.params.id}`
+                    })
+                } else {
+                    return res.status(404).json({
+                        message: `Usuario ${req.params.id} no encontrado`
+                    })
+                }
+            }
+        ).catch(
+            (error) => {
+                return res.status(500).json({
+                    message: "Ocurrió un error interno con la base de datos",
+                    error: error?.toString()
+                })
+            }
+        )
 
-        if(result.modifiedCount > 0) {
-            return res.status(200).json({
-                message: `Se actualizó la información del usuario ${req.params.id}`
-            })
-        }
-
-        return res.status(404).json({
-            message: `Usuario ${req.params.id} no encontrado`
-        })
     } catch (error) {
         return res.status(500).json({
             message: "Ocurrió un error",
-            error: error
+            error: error?.toString()
         })
     }
 }
