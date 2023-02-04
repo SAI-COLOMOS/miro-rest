fbdbb9dfea734be819a151ab9a13c1fcc8d39208
JonathanGDiaz <morokeidovah@gmail.com>
2023-02-03 23:15:28 -0600

    Eliminación del esquema de horas debido a bug

--- app/models/Card.ts
+++ app/models/Card.ts
@@ -4,32 +4,13 @@ export interface HoursInterface extends Document {
     activity_name: String,
     hours: Number,
     assignation_date: Date,
-    assignation_hour: Date,
     responsible_register: String
 }
 
 export interface CardInterface extends Document {
     provider_register: String,
-    hours: HoursInterface
+    activities: HoursInterface
 }
-
-const HoursSchema = new Schema({
-    activity_name: {
-        type: String,
-    },
-    hours: {
-        type: Number,
-        required: [true, "La cantidad de horas son necesarias es necesario"]
-    },
-    assignation_date: {
-        type: Date,
-        default: Date.now
-    },
-    responsible_register: {
-        type: String,
-        required: [true, "El registro del encargado es necesario"]
-    }
-})
 
 const CardSchema = new Schema({
     provider_register: {
@@ -37,13 +18,28 @@ const CardSchema = new Schema({
         unique: true,
         required: [true, "El registro del prestador es necesario"]
     },
-    hours: [HoursSchema]
+    activities: [{
+        activity_name: {
+            type: String,
+        },
+        hours: {
+            type: Number,
+            required: [true, "La cantidad de horas son necesarias es necesario"]
+        },
+        assignation_date: {
+            type: Date,
+            default: Date.now
+        },
+        responsible_register: {
+            type: String,
+            required: [true, "El registro del encargado es necesario"]
+        }
+    }]
 }, {
     versionKey: false,
     timestamps: true
 })
 
 const Card = model<CardInterface>("Cards", CardSchema)
-export const Hours = model<HoursInterface>("Hours", HoursSchema)
 
 export default Card
