b15d5b926fb5426eac4e2f5717fb7231adf4f446
JonathanGDiaz <morokeidovah@gmail.com>
2023-02-03 23:16:54 -0600

    Implementación de métodos faltantes

--- app/controllers/Card.controller.ts
+++ app/controllers/Card.controller.ts
@@ -1,29 +1,53 @@
 import { Request, Response } from "express";
 import Card from "../models/Card";
 
-export const getProviderHours = async (req: Request, res: Response) => {
-    return res.status(200).json({
-        message: "hola"
-    })
-}
-
-export const CardPost = async (provider_register: String) => {
+export const getCards = async (req: Request, res: Response) => {
     try {
-        await new Card({ "provider_register": provider_register }).save().then(result => {
-            if (result) {
-                console.log("Si lo creó")
-            } else {
-                console.log("Hubo un error")
-            }
-        }).catch(error => {
-            console.log(error)
-        })
+        const items: number = req.body.items > 0 ? req.body.items : 10
+        const page: number = req.body.page > 0 ? req.body.page - 1 : 0
+
+        await Card.find().sort({ "createdAt": "desc" }).limit(items).skip(page * items).then(
+            result => {
+                if (result.length > 0) {
+                    return res.status(200).json({
+                        message: "Listo",
+                        cards: result
+                    })
+                } else {
+                    res.status(200).json({
+                        message: "Sin resultados"
+                    })
+                }
+            })
     } catch (error) {
-        console.log(error)
+        return res.status(500).json({
+            message: "Ocurrió un error al conectarse con el servidor"
+        })
     }
 }
 
-export const CardPatch = async (req: Request, res: Response) => {
+export const getProviderHours = async (req: Request, res: Response) => {
+    try {
+        await Card.findOne({ "provider_register": req.params.id }).then(result => {
+            if (result) {
+                return res.status(200).json({
+                    message: "Tarjetón de usuario encontrado",
+                    tarjeton: result.activities
+                })
+            } else {
+                return res.status(404).json({
+                    message: `No se encontró el tarjetón del usuario ${req.params.id}`
+                })
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Ocurrio un error al conectarse al servidor"
+        })
+    }
+}
+
+export const CardPost = async (req: Request, res: Response) => {
     if (!req.body.provider_register) {
         return res.status(400).json({
             message: "Faltan datos"
@@ -31,12 +55,39 @@ export const CardPatch = async (req: Request, res: Response) => {
     }
 
     try {
-        await Card.updateOne({ "provider_register": req.body.provider_register }, {
+        await new Card({ "provider_register": req.body.provider_register }).save().then(result => {
+            if (result) {
+                return res.status(201).json({
+                    message: "Se creó el tarjetón del prestador"
+                })
+            } else {
+                return res.status(500).json({
+                    message: "Ocurrió un error interno en la base de datos"
+                })
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Ocurrió un error al connectarse con el servidor",
+            error: error?.toString()
+        })
+    }
+}
+
+export const AddHoursToCard = async (req: Request, res: Response) => {
+    if (!req.body.activity.activity_name || !req.body.activity.hours || !req.body.activity.responsible_register) {
+        return res.status(400).json({
+            message: "Faltan datos"
+        })
+    }
+
+    try {
+        await Card.updateOne({ "provider_register": req.params.id }, {
             $push: {
-                "hours": {
-                    "activity_name": req.body.hours.activity_name,
-                    "hours": req.body.hours.hours,
-                    "responsible_register": req.body.hours.responsible_register
+                "activities": {
+                    "activity_name": req.body.activity.activity_name,
+                    "hours": req.body.activity.hours,
+                    "responsible_register": req.body.activity.responsible_register
                 }
             }
         }).then(result => {
@@ -55,4 +106,38 @@ export const CardPatch = async (req: Request, res: Response) => {
             message: "Ocurrió un error al connectarse con el servidor"
         })
     }
+}
+
+export const RemoveHoursFromCard = async (req: Request, res: Response) => {
+    if (!req.body.activity_name || !req.body.responsible_register) {
+        return res.status(400).json({
+            message: "Faltan datos"
+        })
+    }
+
+    try {
+        await Card.updateOne({ "provider_register": req.params.id }, {
+            $pull: {
+                "activities": {
+                    "activity_name": req.body.activity_name,
+                    "responsible_register": req.body.responsible_register
+                }
+            }
+        }).then(result => {
+            if (result.modifiedCount > 0) {
+                return res.status(200).json({
+                    message: "Se eliminaron las horas del prestador"
+                })
+            } else {
+                return res.status(404).json({
+                    message: "No se encontró la actividad"
+                })
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Ocurrió un error al conectarse al servidor"
+        })
+    }
+
 }
