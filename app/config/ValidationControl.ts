2244147563821cb0582b3069542c7da62fb8dfbe
JonathanGDiaz <morokeidovah@gmail.com>
2023-02-17 01:00:44 -0600

    Cambio de in por of

--- app/config/ValidationControl.ts
+++ app/config/ValidationControl.ts
@@ -2,8 +2,8 @@ export function __ThrowError(message: string) { throw message }
 
 export function __CheckEnum(arr: Array<string>, value: string, field: string) {
     let is_valid: boolean = false
-    for (let str in arr) {
+    for (let str of arr) {
         value === str ? is_valid = true : null
     }
-    is_valid ? null : __ThrowError(`El campo '${field} debe contener uno de los siguientes strings ${arr}'`)
+    is_valid ? null : __ThrowError(`El campo '${field}' debe contener uno de los siguientes strings ${arr}'`)
 }
