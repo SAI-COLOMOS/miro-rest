54fbb474de9d5ec6cf9a75929985833d17e6ab28
JonathanGDiaz <morokeidovah@gmail.com>
2023-02-16 23:59:11 -0600

    Cambio de formato de validaciones

--- app/middleware/ValidationControl.ts
+++
@@ -1,9 +0,0 @@
-export function __ThrowError(message: string) { throw message }
-
-export function __CheckEnum(arr: Array<string>, value: string) {
-    let is_valid: boolean = false
-    for (let str in arr) {
-        value === str ? is_valid = true : null
-    }
-    return is_valid
-}
