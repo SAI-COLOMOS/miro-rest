4cca41935fdcd9ebf0217dc1f2f2106c1aef468c
JonathanGDiaz <morokeidovah@gmail.com>
2023-02-19 14:22:20 -0600

    Implementación de places

--- app/models/Place.ts
+++ app/models/Place.ts
@@ -14,7 +14,6 @@ export interface PlaceInterface extends Document {
 const AreaSchema = new Schema({
     area_identifier: {
         type: String,
-        required: true,
         unique: true,
         index: true
     },
@@ -32,7 +31,6 @@ const AreaSchema = new Schema({
 const PlaceSchema = new Schema({
     place_identifier: {
         type: String,
-        required: true,
         unique: true,
         index: true
     },
@@ -43,12 +41,59 @@ const PlaceSchema = new Schema({
         lowercase: true,
         index: true
     },
-    place_areas: {
-        type: [AreaSchema]
-    }
+    place_areas: [AreaSchema]
 }, {
     versionKey: false,
     timestamps: true
 })
 
-export default model<PlaceInterface>("Places", PlaceSchema)
+PlaceSchema.pre<PlaceInterface>("save", async function (next) {
+
+    if (this.isNew) {
+        let serie = "01"
+
+        const last_identifier = await Place.findOne().sort({ "place_identifier": "desc" }).select("place_identifier")
+
+        if (last_identifier) {
+            const next_identifier: number = Number(last_identifier) + 1
+
+            if (next_identifier < 10) {
+                serie = "0" + next_identifier
+            } else {
+                serie = next_identifier.toString()
+            }
+        }
+
+        this.place_identifier = serie
+    }
+
+    // if (this.isModified("place_areas")) {
+    //     let serie = "001"
+    //     const obj = this.toObject()
+
+    //     const name = obj.place_areas[obj.place_areas.length - 1].area_name
+    //     let arr = this.place_areas.toObject()
+
+    //     obj.place_areas.pop()
+    //     obj.place_areas.sort(function (a: any, b: any) { return b.area_identifier - a.area_identifier })
+
+    //     const last_identifier = obj.place_areas[0]
+    //     const next_identifier = Number(last_identifier.area_identifier) + 1
+    //     if (next_identifier < 10) {
+    //         serie = "00" + next_identifier
+    //     } else if (next_identifier < 100) {
+    //         serie = "0" + next_identifier
+    //     } else {
+    //         serie = next_identifier.toString()
+    //     }
+
+    //     arr[arr.findIndex((area: AreaInterface) => area.area_name === name)].area_identifier = serie
+    //     this.place_areas = arr
+    // }
+
+    next()
+})
+
+const Place = model<PlaceInterface>("Places", PlaceSchema)
+
+export default Place
--- app/routes/Place.routes.ts
+++ app/routes/Place.routes.ts
@@ -1,13 +1,23 @@
 import { Router } from "express"
 import Passport from "passport"
-import { addArea, PlacePost } from "../controllers/Place.controller"
-import { isAdministradorOrEncargado } from "../middleware/RoleControl"
+import { addArea, getPlace, getPlaces, updatePlace, postPlace, removeArea, updateArea } from "../controllers/Place.controller"
+import { isAdministrador, isAdministradorOrEncargado } from "../middleware/RoleControl"
 
 const route = Router()
 const path = "/places"
 
-route.post(`${path}`, Passport.authenticate('jwt', { session: false }), isAdministradorOrEncargado, PlacePost)
+route.get(`${path}`, Passport.authenticate('jwt', { session: false }), isAdministradorOrEncargado, getPlaces)
 
-route.post(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministradorOrEncargado, addArea)
+route.get(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministradorOrEncargado, getPlace)
+
+route.post(`${path}`, Passport.authenticate('jwt', { session: false }), isAdministrador, postPlace)
+
+route.patch(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministrador, updatePlace)
+
+route.patch(`${path}/:id/:id2`, Passport.authenticate('jwt', { session: false }), isAdministrador, updateArea)
+
+route.post(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministrador, addArea)
+
+route.delete(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministrador, removeArea)
 
 export default route
