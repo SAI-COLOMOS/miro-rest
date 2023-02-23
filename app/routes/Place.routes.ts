64208bd0b31cb4c4c731648bef50506900dc3a20
JonathanGDiaz <morokeidovah@gmail.com>
2023-02-22 23:56:26 -0600

    Actualización de rutas

--- app/routes/Place.routes.ts
+++ app/routes/Place.routes.ts
@@ -7,18 +7,25 @@ import { isAdministrador, isAdministradorOrEncargado } from "../middleware/RoleC
 const route = Router()
 const path = "/places"
 
+// Obtener todas los lugares
 route.get(`${path}`, Passport.authenticate('jwt', { session: false }), isAdministradorOrEncargado, getPlaces)
 
+// Obtener un lugar en específico
 route.get(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministradorOrEncargado, getPlace)
 
+// Crear un lugar
 route.post(`${path}`, Passport.authenticate('jwt', { session: false }), isAdministrador, postPlace)
 
+// Actualizar la información de un lugar
 route.patch(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministrador, updatePlace)
 
+// Actualizar la información de un área
 route.patch(`${path}/:id/:id2`, Passport.authenticate('jwt', { session: false }), isAdministrador, updateArea)
 
+// Crear un área
 route.post(`${path}/:id`, Passport.authenticate('jwt', { session: false }), isAdministrador, addArea)
 
+// Borrar un área
 route.delete(`${path}/:id/:id2`, Passport.authenticate('jwt', { session: false }), isAdministrador, removeArea)
 
 export default route
