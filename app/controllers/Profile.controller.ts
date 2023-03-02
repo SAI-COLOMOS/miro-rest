b4b0e08610cc402cdf61e8978b24d3896bb2a301
JonathanGDiaz <morokeidovah@gmail.com>
2023-03-01 20:27:31 -0600

    Implementación de feed

--- app/controllers/Profile.controller.ts
+++ app/controllers/Profile.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express"
 import User from "../models/User"
+import Agenda from "../models/Agenda"
+import Card from "../models/Card"
 
-export const ProfileGet = async (req: Request, res: Response) => {
+export const getProfile = async (req: Request, res: Response) => {
     try {
         const user = await User.findOne({ 'register': req.params.id })
 
@@ -16,6 +18,38 @@ export const ProfileGet = async (req: Request, res: Response) => {
     } catch (error) {
         return res.status(500).json({
             message: "Ocurrió un error en el servidor",
+            error: error?.toString()
+        })
+    }
+}
+
+export const getFeed = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findOne({ 'register': req.params.id })
+
+        if (user) {
+            const events = await Agenda.find({ "attendance.attendee_list.attendee_register": req.params.id, "attendance.status": "Disponible" }).sort({ "createdAt": "desc" })
+
+            const card = await Card.findOne({ "provider_register": user.register })
+
+            return card && events
+                ? res.status(200).json({
+                    message: "Feed lista",
+                    total_hours: card.total_hours,
+                    achieved_hours: card.achieved_hours,
+                    events: events
+                })
+                : res.status(400).json({
+                    message: `No hay información por recuperar para el usuario ${req.params.id}`
+                })
+        }
+
+        return res.status(400).json({
+            message: `No se encontró el usuario ${req.params.id}`
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: `Ocurrió un error en el servidor`,
             error: error?.toString()
         })
     }
