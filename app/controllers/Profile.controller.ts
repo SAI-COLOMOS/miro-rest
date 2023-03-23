ab843005001e19639f5dba071dfbd4b28b7689d0
JonathanGDiaz <morokeidovah@gmail.com>
2023-03-22 21:07:08 -0600

    implementación de eventos disponibles en feed

--- app/controllers/Profile.controller.ts
+++ app/controllers/Profile.controller.ts
@@ -8,6 +8,7 @@ interface Request_body {
   events: AgendaInterface[]
   achieved_hours?: number
   total_hours?: number
+  available_events?: Array<object>
 }
 
 export const getProfile = async (req: Request, res: Response): Promise<Response> => {
@@ -34,21 +35,45 @@ export const getFeed = async (req: Request, res: Response): Promise<Response> =>
   try {
     const user = new User(req.user)
 
-    const events = await Agenda.find({ "attendance.attendee_list.attendee_register": user.register, "attendance.status": "Disponible" }).sort({ "createdAt": "desc" })
+    const registeredEvents: AgendaInterface[] = await Agenda.find({
+      "attendance.attendee_list.attendee_register": user.register,
+      "attendance.status": "Disponible"
+    }).sort({ "createdAt": "desc" })
 
     const response_body: Request_body = {
       message: "Feed lista",
-      events
+      events: registeredEvents
     }
 
     if (user.role === "Prestador") {
       const card = await Card.findOne({ "provider_register": user.register })
 
+      const events: AgendaInterface[] = await Agenda.find({
+        "belonging_place": user.place,
+        "belonging_area": user.assigned_area,
+        "attendance.status": "Disponible",
+        "attendance.attendee_list.attendee_register": { $not: { $regex: user.register } }
+      }).sort({ "createdAt": "desc" })
+
+      const filteredEvents: AgendaInterface[] = events.filter((event: AgendaInterface) => event.vacancy > event.attendance.attendee_list.length)
+
+      const availableEvents: Array<object> = []
+
+      filteredEvents.forEach((event: AgendaInterface) => {
+        availableEvents.push({
+          "name": event.name,
+          "starting_date": event.starting_date.toISOString(),
+          "place": event.place
+        })
+      })
+
       response_body.achieved_hours = card?.achieved_hours
       response_body.total_hours = card?.total_hours
+      response_body.available_events = availableEvents
     }
 
     return res.status(200).json(response_body)
+
   } catch (error) {
     return res.status(500).json({
       message: `Ocurrió un error en el servidor`,
