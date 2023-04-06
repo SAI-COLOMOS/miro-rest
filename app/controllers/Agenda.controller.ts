ad965de3f7c6143ae408f11e4076e41c77d5c8f7
JonathanGDiaz <morokeidovah@gmail.com>
2023-04-05 21:06:01 -0600

    Feat: Implementación filtrado para prestadores

--- app/controllers/Agenda.controller.ts
+++ app/controllers/Agenda.controller.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express"
-import Agenda, { AgendaInterface } from "../models/Agenda"
-import Card, { CardInterface } from "../models/Card"
-import User, { UserInterface } from "../models/User"
-import Environment from "../config/Environment"
+import { Request, Response } from 'express'
+import Agenda, { AgendaInterface, AttendeeInterface } from '../models/Agenda'
+import Card, { CardInterface } from '../models/Card'
+import User, { UserInterface } from '../models/User'
+import Environment from '../config/Environment'
 import schedule from 'node-schedule'
-import { mensaje, sendEmail } from "../config/Mailer"
-import { __ThrowError, __Query, __Required, __Optional } from "../middleware/ValidationControl"
+import { mensaje, sendEmail } from '../config/Mailer'
+import { __ThrowError, __Query, __Required, __Optional } from '../middleware/ValidationControl'
 
 export const getAgenda = async (req: Request, res: Response): Promise<Response> => {
   try {
@@ -14,15 +14,14 @@ export const getAgenda = async (req: Request, res: Response): Promise<Response> 
 
     const currentDate = new Date()
     const user: UserInterface = new User(req.user)
-    const history: boolean = Boolean(req.query.history)
+    const history: boolean = Boolean(String(req.query.history).toLowerCase() === 'true')
     const avatar: boolean = Boolean(req.query.avatar)
     const items: number = Number(req.query.items) > 0 ? Number(req.query.items) : 10
     const page: number = Number(req.query.page) > 0 ? Number(req.query.page) - 1 : 0
     let filter_request = req.query.filter ? JSON.parse(String(req.query.filter)) : {}
     filter_request.starting_date = { $gt: currentDate }
 
-    if (history)
-      delete filter_request.starting_date
+    if (history) delete filter_request.starting_date
 
     if (req.query.search)
       filter_request = { ...filter_request, $or: [{ "name": { $regex: req.query.search, $options: "i" } }] }
@@ -30,15 +29,16 @@ export const getAgenda = async (req: Request, res: Response): Promise<Response> 
     if (user.role === 'Encargado') {
       filter_request.belonging_area = user.assigned_area
       filter_request.belonging_place = user.place
+      filter_request['attendance.status'] = { $not: { $regex: "Concluido" } }
     }
 
     if (user.role === 'Prestador') {
       filter_request.belonging_place = user.place
       filter_request.belonging_area = user.assigned_area
-      filter_request['attendance.status'] = 'Disponible'
+      filter_request['attendance.status'] = { $not: { $regex: "Concluido" } }
     }
 
-    const events: AgendaInterface[] = await Agenda.find(filter_request).sort({ "starting_date": "desc" }).limit(items).skip(page * items)
+    const events: AgendaInterface[] = await Agenda.find(filter_request).sort({ "starting_date": "asc" }).limit(items).skip(page * items)
     if (events.length === 0) return res.status(200).json({ message: 'Listo', events })
 
     const result: object[] = []
@@ -52,6 +52,17 @@ export const getAgenda = async (req: Request, res: Response): Promise<Response> 
         result.push(objEvent)
       })
     }
+
+    if (user.role === 'Prestador') {
+      const filteredEvents: AgendaInterface[] = events.filter((event: AgendaInterface) => {
+        const list: AttendeeInterface[] = event.attendance.attendee_list
+        const registered: boolean = list.some((attendee: AttendeeInterface) => attendee.attendee_register === user.register)
+        return registered || event.attendance.status === 'Disponible'
+      })
+
+      return res.status(200).json({ message: "Listo", events: filteredEvents })
+    }
+
     return res.status(200).json({ message: "Listo", events: result })
   } catch (error) {
     const statusCode: number = typeof error === 'string' ? 400 : 500
@@ -65,17 +76,21 @@ export const getEvent = async (req: Request, res: Response): Promise<Response> =
   try {
     const avatar: boolean = Boolean(req.query.avatar)
     const event = await Agenda.findOne({ "event_identifier": req.params.id })
+    if (!event) return res.status(400).json({ message: `No se encontró el evento ${req.params.id}` })
 
-    if (avatar && event) return res.status(200).json({ avatar: event.avatar })
+    if (avatar) return res.status(200).json({ avatar: event.avatar })
 
-    return event
-      ? res.status(200).json({
-        message: "Listo",
-        event: event
-      })
-      : res.status(400).json({
-        message: `No se encontró el evento ${req.params.id}`
-      })
+    let list: number = 0
+    event.attendance.attendee_list.forEach((attendee: AttendeeInterface) => {
+      if (attendee.status === 'Inscrito') list++
+    })
+
+    const mutatedEvent = { ...event.toObject(), registered_users: list }
+
+    return res.status(200).json({
+      message: "Listo",
+      event: mutatedEvent
+    })
   } catch (error) {
     return res.status(500).json({
       message: "Ocurrió un error en el servidor",
@@ -109,6 +124,8 @@ export const createEvent = async (req: Request, res: Response): Promise<Response
 
     const currentDate: Date = new Date()
     if (event.publishing_date < currentDate) {
+      event.attendance.status = 'Disponible'
+      event.save()
       const users = await User.find({ "status": "Activo", "role": "Prestador" })
       const from = `"SAI" ${Environment.Mailer.email}`
       const subject = '¡Hay un evento disponible para tí!'
@@ -117,12 +134,14 @@ export const createEvent = async (req: Request, res: Response): Promise<Response
         await sendEmail(from, user.email, subject, body)
       }
     } else {
-      emailNotifications(event.event_identifier, event.publishing_date.toISOString(), event.name)
+      emailNotifications(event.event_identifier, event.publishing_date.toISOString())
     }
 
     const time: Date = event.ending_date
     time.setHours(time.getHours() + 1)
     endEvent(event.event_identifier, event.author_name, time.toISOString())
+
+    startingEvent(event.event_identifier, event.starting_date.toISOString())
 
     return res.status(201).json({ message: "Evento creado" })
   } catch (error) {
@@ -134,6 +153,11 @@ export const createEvent = async (req: Request, res: Response): Promise<Response
 
 export const updateEvent = async (req: Request, res: Response): Promise<Response> => {
   try {
+    Object.keys(req.body).forEach((key: string) => {
+      if (req.body[key] === "")
+        delete req.body[key]
+    })
+
     if (req.body.event_identifier)
       __ThrowError("El campo 'event_identifier' no se puede modificar")
 
@@ -149,7 +173,6 @@ export const updateEvent = async (req: Request, res: Response): Promise<Response
     if (req.body.attendance)
       __ThrowError("El campo 'attendance' no se puede modificar desde éste endpoint")
 
-    __Optional(req.body.is_template, `is_template`, `boolean`, null)
     __Optional(req.body.tolerance, `tolerance`, `number`, null)
     __Optional(req.body.name, `name`, `string`, null)
     __Optional(req.body.description, `description`, `string`, null)
@@ -167,35 +190,43 @@ export const updateEvent = async (req: Request, res: Response): Promise<Response
 
     const result = await Agenda.updateOne({ "event_identifier": req.params.id }, req.body)
 
-    if (result.modifiedCount > 0) {
-      const event: AgendaInterface | null = req.body.ending_date || req.body.publishing_date
-        ? await Agenda.findOne({ "event_identifier": req.params.id })
-        : null
+    if (result.modifiedCount <= 0) return res.status(400).json({ message: `No se encontró el evento ${req.params.id}` })
 
-      let time: Date
+    const event: AgendaInterface | null = req.body.ending_date || req.body.publishing_date
+      ? await Agenda.findOne({ "event_identifier": req.params.id })
+      : null
 
-      if (event && req.body.publishing_date) {
-        time = event.publishing_date
-        time.setHours(time.getHours() - 1)
-        schedule.cancelJob(event.event_identifier)
-        emailNotifications(event.event_identifier, time.toISOString(), event.name)
+    if (event && req.body.publishing_date) {
+      schedule.cancelJob(event.event_identifier)
+      const currentDate: Date = new Date()
+      if (event.publishing_date < currentDate) {
+        event.attendance.status = 'Disponible'
+        event.save()
+        const users = await User.find({ "status": "Activo", "role": "Prestador" })
+        const from = `"SAI" ${Environment.Mailer.email}`
+        const subject = '¡Hay un evento disponible para tí!'
+        const body = mensaje(`La inscripción para el evento  ${event.name} empieza en una hora.`)
+        for (const user of users) {
+          await sendEmail(from, user.email, subject, body)
+        }
+      } else {
+        emailNotifications(event.event_identifier, event.publishing_date.toISOString())
       }
-
-      if (event && req.body.ending_date) {
-        time = event.ending_date
-        time.setHours(time.getHours() + 1)
-        schedule.cancelJob(`end_${event.event_identifier}`)
-        endEvent(event.event_identifier, event.author_name, time.toISOString())
-      }
-
-      return res.status(200).json({
-        message: `Se actualizó la información del evento ${req.params.id}`
-      })
     }
 
-    return res.status(400).json({
-      message: `No se encontró el evento ${req.params.id}`
-    })
+    if (event && req.body.ending_date) {
+      const time: Date = event.ending_date
+      time.setHours(time.getHours() + 1)
+      schedule.cancelJob(`end_${event.event_identifier}`)
+      endEvent(event.event_identifier, event.author_name, time.toISOString())
+    }
+
+    if (event && req.body.starting_date) {
+      schedule.cancelJob(`start_${event.event_identifier}`)
+      startingEvent(event.event_identifier, event.starting_date.toISOString())
+    }
+
+    return res.status(200).json({ message: `Se actualizó la información del evento ${req.params.id}` })
   } catch (error) {
     const statusCode: number = typeof error === 'string' ? 400 : 500
     const response: object = statusCode === 400 ? { error } : { message: 'Ocurrió un error en el servidor', error: error?.toString() }
@@ -259,6 +290,7 @@ export const deleteEvent = async (req: Request, res: Response): Promise<Response
     if (result.deletedCount !== 0) {
       schedule.cancelJob(req.params.id)
       schedule.cancelJob(`end_${req.params.id}`)
+      schedule.cancelJob(`start_${req.params.id}`)
 
       return res.status(200).json({
         message: "Evento eliminado"
@@ -276,17 +308,32 @@ export const deleteEvent = async (req: Request, res: Response): Promise<Response
   }
 }
 
-const emailNotifications = async (event_identifier: string, time: string, event_name: string): Promise<void> => {
+const emailNotifications = async (event_identifier: string, time: string): Promise<void> => {
   schedule.scheduleJob(event_identifier, time,
-    async function (name: string) {
+    async function (event_identifier: string) {
+      const event: AgendaInterface | null = await Agenda.findOne({ "event_identifier": event_identifier })
+      if (!event) return
+      event.attendance.status = 'Disponible'
+      event.save()
       const users = await User.find({ "status": "Activo", "role": "Prestador" })
       const from = `"SAI" ${Environment.Mailer.email}`
       const subject = '¡Hay un evento disponible para tí!'
-      const body = mensaje(`La inscripción para el evento  ${name} empieza en una hora.`)
+      const body = mensaje(`La inscripción para el evento  ${event.name} empieza en una hora.`)
       for (const user of users) {
         await sendEmail(from, user.email, subject, body)
       }
-    }.bind(null, event_name)
+    }.bind(null, event_identifier)
+  )
+}
+
+const startingEvent = async (event_identifier: string, time: string) => {
+  schedule.scheduleJob(`start_${event_identifier}`, time,
+    async function (event_identifier: string) {
+      const event: AgendaInterface | null = await Agenda.findOne({ "event_identifier": event_identifier })
+      if (!event) return
+      event.attendance.status = 'En proceso'
+      event.save()
+    }.bind(null, event_identifier)
   )
 }
 
