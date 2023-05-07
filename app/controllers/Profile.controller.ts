f74d1a75c4671396b1bffaba69f499d4dadf9a61
JonathanGDiaz <morokeidovah@gmail.com>
2023-05-07 15:41:55 -0600

    Chore: Adición de campo avatar

--- app/controllers/Profile.controller.ts
+++ app/controllers/Profile.controller.ts
@@ -44,18 +44,18 @@ export const getFeed = async (req: Request, res: Response): Promise<Response> =>
       ]
     }
 
-    const enrolled_events: IEvent[] = await Agenda.find(querySearch, { avatar: 0 }).sort({ "starting_date": "asc" })
+    const enrolled_events: IEvent[] = await Agenda.find(querySearch).sort({ "starting_date": "asc" })
     const responseBody: Request_body = { enrolled_event: enrolled_events.length === 0 ? null : enrolled_events[0] }
 
     if (user.role === 'Prestador') {
-      const card = await Card.findOne({ "provider_register": user.register })
+      const card = await Card.findOne({ "provider_regiister": user.register })
 
       const availableEvents: IEvent[] = await Agenda.find({
         "belonging_place": user.place,
         "belonging_area": user.assigned_area,
         "attendance.status": "Disponible",
         "attendance.attendee_list.attendee_register": { $not: { $regex: user.register } },
-      }, { avatar: 0 }).sort({ "starting_date": "asc" })
+      }).sort({ "starting_date": "asc" })
 
       responseBody.achieved_hours = card?.achieved_hours
       responseBody.total_hours = card?.total_hours
